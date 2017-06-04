9805bf243885df27c18689784d895475cd7580dd
Hung Dao <hung@hoodline.com>
2017-06-04 00:24:03 -0700

    added more sounds

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -1,13 +1,36 @@
 'use strict';
 
-// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
 var audioData = [
   {
     'title': 'Cafe',
-    'url': 'https://raw.githubusercontent.com/zocoi/skill-sample-nodejs-audio-player/mainline/res/audio/cafe.m4a'
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/cafe.m4a'
+  }, {
+    'title': 'Day',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/day.m4a'
+  }, {
+    'title': 'Fire',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/fire.m4a'
+  }, {
+    'title': 'Night',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/night.m4a'
   }, {
     'title': 'Rain',
-    'url': 'https://raw.githubusercontent.com/zocoi/skill-sample-nodejs-audio-player/mainline/res/audio/rain.m4a'
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/rain.m4a'
+  }, {
+    'title': 'River',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/river.m4a'
+  }, {
+    'title': 'Space',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/space.m4a'
+  }, {
+    'title': 'Thunder',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/thunder.m4a'
+  }, {
+    'title': 'River',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/water.m4a'
+  }, {
+    'title': 'Wind',
+    'url': 'https://raw.githubusercontent.com/zocoi/alexa-whitenoise/master/res/audio/wind.m4a'
   }
 ];
 
